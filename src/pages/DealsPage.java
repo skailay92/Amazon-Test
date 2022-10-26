fdc01d302022d73d33f88dbf60d732490217a3d5
shingara kailay <skailay92@gmail.com>
2022-10-26 15:17:01 -0400

    add deals page

---
+++ src/pages/DealsPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class DealsPage extends HomePageTest {
+	
+	public void  deals()  {
+		
+				
+	}
+
+}
